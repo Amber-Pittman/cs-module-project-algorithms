f52cf1874d058415aa4dfa97113ceeeaad549a77
Amber-Pittman <amber@amberpittman.com>
2020-07-16 18:34:05 -0400

    Eating cookies now passes both tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,23 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
-    if n == 1 or n == 0:
+    if not cache:
+        cache = {}
+    if n <= 0:
         return 1
-    if n == 2:
+    elif n == 1:
+        return 1
+    elif n == 2:
         return 2
-    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    elif n == 3:
+        return 4
+    elif cache and cache[n]:
+        return cache[n]
+    else:
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
@@ -16,4 +26,4 @@ if __name__ == "__main__":
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
 
-#currently passes the small test
+#Now passes both small and large tests
